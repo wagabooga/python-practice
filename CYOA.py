5b8d8f24f7cf2b691afef69106b3a31a9e905dad
wagabooga <Mattyhutwo@gmail.com>
2020-05-03 16:38:20 -0700

    added create player function

--- CYOA.py
+++ CYOA.py
@@ -1,22 +1,33 @@
+# class that represents inventory
 class Inventory:
-    # class that represents inventory
     def __init__(self):
         self.items = []
 
 
+# class that keeps track of player data
 class Player:
-    #class that keeps track of player data
     def __init__(self,name):
         self.name = name
         self.hp = 10
         self.inventory = Inventory()
 
 
+# this function runs the game loop
 def game_run():
     is_user_playing = True
+    print("Welcome to the game!")
     while is_user_playing == True:
-        #the game runs inside of this loop
-        print("Welcome to the game")
+        player = create_player()
         is_user_playing = False
 
+
+# this function prompts the user for their name
+# and then creates and returns the player
+def create_player():
+    player_name = input("What is your name?")
+    player = Player(player_name)
+    return player
+
+
 game_run()
+
